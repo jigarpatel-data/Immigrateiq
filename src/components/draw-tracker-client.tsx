5a458488769a460fa1d23b18d7ed87ae4730de49
jigar patel <pateljigarr136@gmail.com>
2025-09-24 18:51:58 +0000

    make height relative to screen size (_for element <Card>_)

--- src/components/draw-tracker-client.tsx
+++ src/components/draw-tracker-client.tsx
@@ -355,8 +355,8 @@ export function DrawTrackerClient({
                 </Card>
               </div>
               <div className="relative">
-                <ScrollArea className="h-[calc(100vh-18rem)]" viewportRef={viewportRef} onScroll={handleScroll}>
-                <div className="space-y-4">
+                <ScrollArea className="h-[calc(100vh-24rem)]" viewportRef={viewportRef} onScroll={handleScroll}>
+                <div className="space-y-4 pr-4">
                     {loading && allDraws.length === 0 ? (
                         <div className="flex flex-col items-center justify-center text-center py-16">
                             <Loader2 className="h-12 w-12 animate-spin text-primary mb-4" />
@@ -556,7 +556,7 @@ export function DrawTrackerClient({
                                   </div>
                               </CardHeader>
                               <CardContent>
-                                  <ScrollArea className="h-[calc(100vh-18rem)]">
+                                  <ScrollArea className="h-[calc(100vh-22rem)]">
                                     <DrawDetailsContent />
                                   </ScrollArea>
                               </CardContent>
