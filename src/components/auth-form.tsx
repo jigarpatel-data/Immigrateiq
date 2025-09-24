aeaa858a796857bbbb5e09d8108a9aeb3ebb879f
jigar patel <pateljigarr136@gmail.com>
2025-09-24 15:54:21 +0000

    now at top add anthor image "https://firebasestorage.googleapis.com/v0/b

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -311,7 +311,8 @@ export function AuthForm() {
         <Form {...passwordResetForm}>
           <form onSubmit={passwordResetForm.handleSubmit(onPasswordResetSubmit)}>
             <AlertDialogHeader className="items-center text-center">
-               <Link href="/" className="flex flex-col items-center gap-2">
+               <Link href="/" className="flex flex-col items-center gap-4">
+                <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/android-chrome-192x192.png?alt=media&token=4e79ad3d-2db0-4b6c-bc68-efa3d2633eb8" alt="TheCanIndian Logo" width={64} height={64} />
                 <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/Black%20background-final.png?alt=media&token=9086963b-efba-4599-8ff3-76ca37d7ba1c" alt="TheCanIndian Logo" width={180} height={72} />
               </Link>
               <AlertDialogTitle>Forgot Password?</AlertDialogTitle>
@@ -353,3 +354,4 @@ export function AuthForm() {
     </AlertDialog>
   );
 }
+
