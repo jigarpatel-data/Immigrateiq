66bc52093274051a91c34c56fb8e0ddd2e687dc9
jigar patel <pateljigarr136@gmail.com>
2025-10-09 13:13:27 +0000

    reduce size  for mobile device by 30% (_for element <LinkComponent>_)

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -11,8 +11,8 @@ export default function AuthPage() {
         <div className="w-full max-w-md space-y-6">
           <div className="flex justify-center flex-col items-center gap-2">
             <Link href="/" className="flex flex-col items-center">
-               <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/android-chrome-192x192.png?alt=media&token=4e79ad3d-2db0-4b6c-bc68-efa3d2633eb8" alt="TheCanIndian Logo" width={100} height={100} className="z-10 relative mb-[-30px]" />
-               <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/Black%20background-final.png?alt=media&token=9086963b-efba-4599-8ff3-76ca37d7ba1c" alt="TheCanIndian Logo" width={180} height={72} />
+               <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/android-chrome-192x192.png?alt=media&token=4e79ad3d-2db0-4b6c-bc68-efa3d2633eb8" alt="TheCanIndian Logo" width={70} height={70} className="z-10 relative mb-[-21px]" />
+               <Image src="https://firebasestorage.googleapis.com/v0/b/thecanindian.firebasestorage.app/o/Black%20background-final.png?alt=media&token=9086963b-efba-4599-8ff3-76ca37d7ba1c" alt="TheCanIndian Logo" width={126} height={50} />
             </Link>
              <p className="text-muted-foreground text-center pt-4">
               Welcome! Please sign in or create an account to continue.
